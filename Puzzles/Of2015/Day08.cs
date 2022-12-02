468fc8610ee0d5deeaf024f0803e82994e4fee35
MrBlumi <>
2022-12-02 18:46:02 +0100

    Puzzle 2015, day 8

---
+++ Puzzles/Of2015/Day08.cs
@@ -0,0 +1,29 @@
+namespace AdventOfCode.Puzzles.Of2015;
+
+public sealed record Day08() : Puzzle(Year: 2015, Day: 08, "Matchsticks")
+{
+    protected override int PartOne()
+    {
+        int DecodedLength(ReadOnlySpan<char> input) => input switch
+        {
+            ['\\', 'x', _, _, .. var rest] => 1 + DecodedLength(rest),
+            ['\\', _, .. var rest] => 1 + DecodedLength(rest),
+            [ '"', .. var rest ] => 0 + DecodedLength(rest),
+            [ _, .. var rest ] => 1 + DecodedLength(rest),
+            _ => 0
+        };
+
+        return InputLines
+            .Select(_ => _.Length - DecodedLength(_.AsSpan()))
+            .Sum();
+    }
+
+    protected override int PartTwo()
+    {
+        int EncodedLength(string input) => input.Length + input.Count("\"\\".Contains) + 2;
+
+        return InputLines
+            .Select(_ => EncodedLength(_) - _.Length)
+            .Sum();
+    }
+}
