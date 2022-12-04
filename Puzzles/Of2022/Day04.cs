31034b2af52dc6689002ab962e7c041875fcbb56
MrBlumi <>
2022-12-04 17:12:33 +0100

    Puzzle 2022, day 4

--- Puzzles/Of2022/Day04.cs
+++ Puzzles/Of2022/Day04.cs
@@ -2,20 +2,14 @@ namespace AdventOfCode.Puzzles.Of2022;
 
 public sealed record Day04 : Puzzle
 {
-    private readonly ((int Start, int End) First, (int Start, int End) Second)[] _tuples;
+    private readonly int[][] _tuples;
 
-    public Day04() : base(Year: 2022, Day: 04, "Camp Cleanup") => _tuples = InputLines
-        .Select(_ => _.Split(',', '-').Select(int.Parse).ToArray())
-        .Select(_ => (First: (Start: _[0], End: _[1]), Second: (Start: _[2], End: _[3])))
-        .ToArray();
+    public Day04() : base(Year: 2022, Day: 04, "Camp Cleanup") =>
+        _tuples = InputLines.Select(_ => _.Split(',', '-').Select(int.Parse).ToArray()).ToArray();
     
     private int Solve(Func<bool,bool,bool> evaluate) => _tuples.Count(_ =>
-        evaluate(
-            _.First.Start >=_.Second.Start && _.First.Start <=_.Second.End,
-            _.First.End >=_.Second.Start && _.First.End <= _.Second.End) ||
-        evaluate(
-            _.Second.Start >= _.First.Start && _.Second.Start <= _.First.End,
-            _.Second.End >= _.First.Start && _.Second.End <= _.First.End));
+        evaluate(_[0] >=_[2] && _[0] <=_[3], _[1] >=_[2] && _[1] <= _[3]) ||
+        evaluate(_[2] >= _[0] && _[2] <= _[1], _[3] >= _[0] && _[3] <= _[1]));
 
     protected override int PartOne() => Solve(evaluate: (left, right) => left && right);
 
