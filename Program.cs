e11efccca10eddb6a449cd407b01123c078f2f43
MrBlumi <>
2023-12-01 17:16:36 +0100

    Refactor entry logic

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
 ﻿using System.Reflection;
 
-typeof(Program).Assembly
+const string help = """
+                    --year      the year of the puzzle
+                    --day       the day of the puzzle
+                    --help      print this information
+                    """;
+
+if (args is [] || args.Contains("--help"))
+{
+    Console.WriteLine(help);
+    return;
+}
+
+var year = Parse(args[Array.IndexOf(args, "--year") + 1]);
+var day = Parse(args[Array.IndexOf(args, "--day") + 1]);
+
+var puzzle = typeof(Program).Assembly
     .GetTypes()
     .Where(_ => _.IsAssignableTo(typeof(IPuzzle)) && _.IsAbstract == false)
     .Select(_ => (Type: _, Attribute: _.GetCustomAttribute<PuzzleAttribute>()!))
-    .OrderBy(_ => _.Attribute.Year)
-    .ThenBy(_ => _.Attribute.Day)
+    .Where(_ => _.Attribute.Year == year && _.Attribute.Day == day)
     .Select(_ => Activator.CreateInstance(_.Type))
     .Cast<IPuzzle>()
-    .ForEach(_ => _.Solve());
+    .Single();
+
+puzzle.Solve();
